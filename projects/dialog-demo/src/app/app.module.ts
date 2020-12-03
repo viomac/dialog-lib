bb716b1f0c4b47ae1c29476a271c6e814ca54b48
vime <vime@sofico.be>
2020-12-03 12:04:12 -0600

    Connect demo app with the library

--- projects/dialog-demo/src/app/app.module.ts
+++ projects/dialog-demo/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogLibModule } from '../../../dialog-lib/src/lib/dialog-lib.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DialogLibModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
