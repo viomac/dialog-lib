fdd14270b97faf0ca7aa83179de7519833bb4655
vime <vime@sofico.be>
2020-12-07 17:59:01 -0600

    Update API

--- projects/dialog-lib/src/public-api.ts
+++ projects/dialog-lib/src/public-api.ts
@@ -2,8 +2,10 @@
  * Public API Surface of dialog-lib
  */
 
-export * from './lib/dialog-lib.service';
+export * from './lib/dialog-injector';
 export * from './lib/dialog-lib.component';
 export * from './lib/dialog-lib.config';
-export * from './lib/dialog-ref';
 export * from './lib/dialog-lib.module';
+export * from './lib/dialog-lib.service';
+export * from './lib/dialog-ref';
+export * from './lib/insertion.directive';
